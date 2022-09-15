a24188df092bc2f6d48d56433b607b924b814bff
MC4002 <maygec@nycstudents.net>
2022-09-15 12:30:12 -0400

    09/15

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,85 +1,133 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<div>
+    <h1>TBD</h1>
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+export default {
+    name: "TBD",
+    components: {},
+    data () {
+        return {
+            Scale: "https://th.bing.com/th/id/R.4c9136e2c6df028d3eccaf398fa2cd29?rik=uYxj7Y5dnSTKWw&riu=http%3a%2f%2fbagha.ca%2fwp-content%2fuploads%2f2015%2f07%2fpiano3-small.png&ehk=L%2f3sy6TWG%2fFflU5rYmw8snStzxSFadv%2bAP%2b2Reqf140%3d&risl=&pid=ImgRaw&r=0",
+            Notes: [
+                {
+                    Note: "C",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/R.4a38845654d2e3487c0be4fc535291d0?rik=jvkK3hdbr4OhVA&riu=http%3a%2f%2fxoax.net%2fchildren%2fref%2fmusic%2fincl%2fscales_octaves%2fmiddle_c_treble_clef.png&ehk=aYg3F0pjbwYRjhTqMWhLM1SIV6kYuTYoBE%2bFwK5vPuM%3d&risl=&pid=ImgRaw&r=0",
+                },
+                {
+                    Note: "D",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.WQlmqSeuSnkNchHJrIyIEAAAAA?w=155&h=134&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "E",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.lgUKproo0AbGYQjAyc71iAHaEc?w=264&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "F",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.d_YkVpbFqsZrXB0e_tp8ywAAAA?w=157&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "G",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.EhPQgF81Dm_A-5vs8nsbfAHaEz?w=243&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "A",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.bOM7aad2o7CjQOPJaYsLjAAAAA?w=158&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "B",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.1AbdfTLv1FkESxCH2hKBIwHaGr?w=192&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "C",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.jwotOjW2UItdMzhlMTZ4ogAAAA?w=200&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "D",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.0oKIaW4RldPn1_tREKds5QHaGr?w=176&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "E",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.1QV6r5KzX_y42TLx2p-LOQHaGr?w=179&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "F",
+                    Clef: "Treble",
+                    Img: "https://th.bing.com/th/id/OIP.2H-SrOenmgtUyKX0P16Y6gHaGr?w=174&h=180&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "C",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.108ZW2BZHPaGKlC5xRngPAAAAA?w=189&h=106&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "B",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.ZZT6zOTzk5SdMUjmZ-0q5wAAAA?w=140&h=154&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "A",
+                    Clef: "Bass",
+                    Img: "https://www.quia.com/files/quia/users/troykay5/musicnotesbass/A_1",
+                },
+                {
+                    Note: "G",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.Sfv4XrN_OhyQtC5D7fbfdwHaDt?w=317&h=174&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "F",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.gvBCFlJ5zSFhOnbTczpM1gHaCJ?w=320&h=101&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "E",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.k8VBSX39KIXZFRrxUFAu9gHaFt?w=224&h=179&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "D",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.LjzkuoAXvTo3HNlOX6399gHaDt?w=299&h=174&c=7&r=0&o=5&pid=1.7",
+                },
+                {
+                    Note: "C",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.uxPi007mRerlCpJR6KzNxQHaFt?w=202&h=179&c=7&r=0&o=5&pid=1.7",
+                },
+                 {
+                    Note: "B",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.iZscitKAYFRLZ9gN5gQ0TQAAAA?w=224&h=179&c=7&r=0&o=5&pid=1.7",
+                },
+             {
+                    Note: "A",
+                    Clef: "Bass",
+                    Img: "https://th.bing.com/th/id/OIP.eoCNFUK2hzVnK42oV-PH0AAAAA?w=209&h=179&c=7&r=0&o=5&pid=1.7",
+                },
+            ],
+        }
+    },
+    }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+h1 {
+    margin: 0px;
+    padding: 0px;
+    font-size: 2rem;
 }
 </style>
---
+++ test/src/components/icons/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="Notes">
+    <div class="Note-Card">
+      <h2>{{ Note }}</h2>
+      <h2>{{ Clef }}</h2>
+      <img class="Img" :src="Image" alt="Note" />      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    Note: String,
+    Clef: String,
+    Image: String,
+  },
+};
+</script>
+
+<style>
+.Img {
+  width: 37%;
+}
+.Note-Card {
+  width: 30%;
+  height: 45%;
+  margin: 2rem;
+  align-content: center;
+}
+.Notes {
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+</style>
